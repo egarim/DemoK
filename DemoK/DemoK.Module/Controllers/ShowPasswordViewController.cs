36be43e1882bda49ba86d62081b69c322eebcb9e
Jose Ojeda <joche.ojeda@bitframeworks.com>
2025-08-28 16:42:59 -0600

    changemodel

--- DemoK/DemoK.Module/Controllers/ShowPasswordViewController.cs
+++ DemoK/DemoK.Module/Controllers/ShowPasswordViewController.cs
@@ -1,4 +1,5 @@
 ﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Model;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -51,6 +52,9 @@ namespace DemoK.Module.Controllers
                 // Handle the ShowPassword property change here
                 // You can add your custom logic here
                 System.Diagnostics.Debug.WriteLine($"ShowPassword changed to: {customLogonParams?.ShowPassword}");
+                
+                // Update the Password property editor caption
+                UpdatePasswordCaption(customLogonParams?.ShowPassword ?? false);
             }
         }
         
@@ -58,6 +62,58 @@ namespace DemoK.Module.Controllers
         {
             base.OnViewControlsCreated();
             // Access and customize the target View control.
+        }
+        
+        /// <summary>
+        /// Updates the Password property editor caption based on the ShowPassword value
+        /// </summary>
+        /// <param name="showPassword">The current value of ShowPassword property</param>
+        private void UpdatePasswordCaption(bool showPassword)
+        {
+            try
+            {
+                // Access the view model
+                var detailViewModel = View.Model as IModelDetailView;
+                if (detailViewModel == null)
+                {
+                    System.Diagnostics.Debug.WriteLine("Unable to access detail view model");
+                    return;
+                }
+
+                // Find the Password property editor model
+                var passwordPropertyEditor = detailViewModel.Items["Password"] as IModelPropertyEditor;
+                if (passwordPropertyEditor == null)
+                {
+                    System.Diagnostics.Debug.WriteLine("Password property editor not found in model");
+                    return;
+                }
+
+                // Following the DevExpress pattern for applying model changes immediately
+                // https://docs.devexpress.com/eXpressAppFramework/118592
+                
+                // Obtain and save the view
+                var savedView = View;
+
+                // Detach the View from the Frame - Don't dispose the old view
+                if (Frame.SetView(view: null, true, null, disposeOldView: false))
+                {
+                    // Change the Application Model
+                    string newCaption = $"Password - {showPassword.ToString().ToLower()}";
+                    ((IModelViewItem)passwordPropertyEditor).Caption = newCaption;
+                    
+                    System.Diagnostics.Debug.WriteLine($"Updated Password caption to: {newCaption}");
+
+                    // Load Model changes into the View
+                    savedView.LoadModel(false);
+
+                    // Re-attach the View back to its Frame
+                    Frame.SetView(savedView);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Error updating password caption: {ex.Message}");
+            }
         }
     }
 }
