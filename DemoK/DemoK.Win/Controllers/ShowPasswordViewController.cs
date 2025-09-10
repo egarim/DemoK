3a0e4c15efa9869a75b6f5c35dd3ca053d8b7fda
Jose Ojeda <joche.ojeda@bitframeworks.com>
2025-09-10 16:27:54 -0600

    clean

--- DemoK/DemoK.Win/Controllers/ShowPasswordViewController.cs
+++ DemoK/DemoK.Win/Controllers/ShowPasswordViewController.cs
@@ -59,14 +59,12 @@ namespace DemoK.Module.Controllers
                 System.Diagnostics.Debug.WriteLine($"ShowPassword changed to: {customLogonParams?.ShowPassword}");
 
 
-                //var PasswordPropertyEditor=  this.View.FindItem("Password") as PropertyEditor;
-                //PasswordPropertyEditor.IsPassword = !(customLogonParams?.ShowPassword ?? false);
+            
 
-                UpdatePasswordCaption(!customLogonParams.ShowPassword);
+                ShowPassword(!customLogonParams.ShowPassword);
 
 
-                // Update the Password property editor caption
-                //UpdatePasswordCaption(customLogonParams?.ShowPassword ?? false);
+              
             }
         }
         
@@ -76,23 +74,12 @@ namespace DemoK.Module.Controllers
             // Access and customize the target View control.
           
         }
-        private void SetMinValue(ViewItem viewItem)
-        {
-            var spinEdit = viewItem.Control as DevExpress.XtraEditors.TextEdit;
-            var dxeditor= viewItem as PropertyEditor;
-            dxeditor.IsPassword = false;
-
-            //if (spinEdit != null) {
-            //    spinEdit.Properties.UseSystemPasswordChar = false;
-            //}
-          
-
-        }
+    
         /// <summary>
         /// Updates the Password property editor caption and IsPassword behavior based on the ShowPassword value
         /// </summary>
         /// <param name="showPassword">The current value of ShowPassword property</param>
-        private void UpdatePasswordCaption(bool showPassword)
+        private void ShowPassword(bool showPassword)
         {
             try
             {
@@ -126,20 +113,13 @@ namespace DemoK.Module.Controllers
                 // Detach the View from the Frame - Don't dispose the old view
                 if (Frame.SetView(view: null, true, null, disposeOldView: false))
                 {
-                    // Change the Application Model - Update caption
-                    string newCaption = $"Password - {showPassword.ToString().ToLower()}";
-                    IModelViewItem passwordPropertyEditor1 = ((IModelViewItem)passwordPropertyEditor);
-                    passwordPropertyEditor1.Caption = newCaption;
 
+
+
+                    //Model https://docs.devexpress.com/eXpressAppFramework/DevExpress.ExpressApp.Editors.PropertyEditor.IsPassword
                     IModelCommonMemberViewItem p = passwordPropertyEditor as IModelCommonMemberViewItem;
                     p.IsPassword = showPassword;
 
-
-                    //var passwordEditor = passwordPropertyEditor as StringPropertyEditor;
-                    //passwordEditor.IsPassword = showPassword;
-
-                    //UpdatePasswordEditorBehavior(showPassword);
-                    System.Diagnostics.Debug.WriteLine($"Updated Password caption to: {newCaption}");
 
                     // Load Model changes into the View
                     savedView.LoadModel(false);
@@ -157,55 +137,6 @@ namespace DemoK.Module.Controllers
             }
         }
 
-        /// <summary>
-        /// Updates the StringPropertyEditor IsPassword property to control password masking
-        /// </summary>
-        /// <param name="showPassword">The current value of ShowPassword property</param>
-        private void UpdatePasswordEditorBehavior(bool showPassword)
-        {
-            try
-            {
-                // Cast the View to DetailView to access the Items collection
-                var detailView = View as DetailView;
-                if (detailView == null)
-                {
-                    System.Diagnostics.Debug.WriteLine("View is not a DetailView");
-                    return;
-                }
-
-                // Find the actual Password property editor control
-                var passwordEditor = detailView.FindItem("Password") as StringPropertyEditor;
-                if (passwordEditor != null)
-                {
-
-                    // Check if the property editor has an IsPassword property
-                    var isPasswordProperty = passwordEditor.GetType().GetProperty("IsPassword");
-                    if (isPasswordProperty != null && isPasswordProperty.CanWrite)
-                    {
-                        // Set IsPassword to the inverse of showPassword
-                        // When showPassword is true, we want the field to show text (IsPassword = false)
-                        // When showPassword is false, we want the field to be masked (IsPassword = true)
-                        bool newIsPasswordValue = showPassword;
-
-                        passwordEditor.IsPassword = newIsPasswordValue;
-                        //isPasswordProperty.SetValue(passwordEditor, newIsPasswordValue);
-
-                        System.Diagnostics.Debug.WriteLine($"Updated Password IsPassword property to: {newIsPasswordValue}");
-                    }
-                    else
-                    {
-                        System.Diagnostics.Debug.WriteLine("IsPassword property not found or not writable on the property editor");
-                    }
-                }
-                else
-                {
-                    System.Diagnostics.Debug.WriteLine("PropertyEditor for Password not found");
-                }
-            }
-            catch (Exception ex)
-            {
-                System.Diagnostics.Debug.WriteLine($"Error updating password editor behavior: {ex.Message}");
-            }
-        }
+      
     }
 }
